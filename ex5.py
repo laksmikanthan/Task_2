78506708f784cdd7dc6d861660daa77ca21311f6
laksmikanthan <mlaksmikanthan2003@gmail.com>
2024-04-27 12:00:55 +0530

    updated

---
+++ ex5.py
@@ -0,0 +1,15 @@
+import json
+
+# Read the JSON file and load it into a dictionary
+with open('ex5.json', 'r') as file:
+    ex5 = json.load(file)
+
+# Add a new batter named "Tea" for the donut with the name "Old Fashioned"
+for donut in ex5:
+    if donut["name"] == "Old Fashioned":
+        donut["batters"]["batter"].append({"id": "1005", "type": "Tea"})
+        break
+
+#  updated dictionary 
+with open('ex5.json', 'w') as file:
+    json.dump(ex5, file, indent=4)
