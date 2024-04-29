9e61cbb7d0a30beb451ed9c2f42618d557a22ed7
LAKSMI KANTHAN M <160707131+laksmikanthan@users.noreply.github.com>
2024-04-29 22:21:39 +0530

    Update ex5.py

--- Assesment2/ex5.py
+++ Assesment2/ex5.py
@@ -7,7 +7,7 @@ with open('ex5.json', 'r') as file:
 # Add a new batter named "Tea" for the donut with the name "Old Fashioned"
 for donut in ex5:
     if donut["name"] == "Old Fashioned":
-        donut["batters"]["batter"].append({"id": "1005", "type": "Tea"})
+        donut["batters"]["batter"].append({"id": "1003", "type": "Tea"})
         break
 
 #  updated dictionary 
