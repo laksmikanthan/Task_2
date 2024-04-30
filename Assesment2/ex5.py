e444b997b73fa289a18f72b5cb008d8e42cd3b4f
laksmikanthan <mlaksmikanthan2003@gmail.com>
2024-04-30 12:01:08 +0530

    updated

--- Assesment2/ex5.py
+++ Assesment2/ex5.py
@@ -1,13 +1,13 @@
 import json
 
-# Read the JSON file and load it into a dictionary
-with open('ex5.json', 'r') as file:
+# Read the JSON file 
+with open('Assesment2/ex5.json', 'r') as file:
     ex5 = json.load(file)
 
-# Add a new batter named "Tea" for the donut with the name "Old Fashioned"
+# Add a new batter 
 for donut in ex5:
     if donut["name"] == "Old Fashioned":
-        donut["batters"]["batter"].append({"id": "1005", "type": "Tea"})
+        donut["batters"]["batter"].append({"id": "1003", "type": "Tea"})
         break
 
 #  updated dictionary 
